911e168b6e51c180b5a0f73650dde6b715954eb5
nazimrasheed <mr.rasheed1991@gmail.com>
2021-08-11 20:41:37 +0100

    added the folder structure

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,3 +1,4 @@
+const { Model } = require("mongoose");
 const Workout = require("../../models/workout");
 
 const getLastWorkout = async (req, res) => {
@@ -11,5 +12,54 @@ const getLastWorkout = async (req, res) => {
         },
       },
     ]);
-  } catch (error) {}
+    res.json(workout);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+const addExercises = async (req, res) => {
+  try {
+    const workout = await Workout.findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body },
+    });
+    res.json(workout);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+const createWorkout = async (req, res) => {
+  try {
+    const workout = await Workout.create({});
+    res.json(workout);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+const getWorkoutsRange = async (req, res) => {
+  try {
+    const workout = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ])
+      .sort({ _id: -1 })
+      .limit(7);
+    res.json(workout);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+module.exports = {
+  getLastWorkout,
+  addExercises,
+  createWorkout,
+  getWorkoutsRange,
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
+const {
+  getLastWorkout,
+  addExercises,
+  createWorkout,
+  getWorkoutsRange,
+} = require("../../controller/api/index");
 
 //require in the controller function
 router.get("/api/workouts", getLastWorkout);
 
-router.put("/api/workouts/:id", addExercise);
+router.put("/api/workouts/:id", addExercises);
 
 router.post("/api/workouts", createWorkout);
 
